8b22b3341d9ecc2c9cbd01cdbd1dd878942bd412
Renato Souza <renato.souza@kumulus.com.br>
2020-06-09 15:46:28 -0300

    Configurando Azure Redis Cache

---
+++ TestRedisAzure/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace TestRedisAzure
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			IDatabase database = lazyConnection.Value.GetDatabase();
+			
+			Console.WriteLine(database.StringGet("Kumulus"));
+			database.StringSet("Kumulus", "Test Redis");
+			Console.WriteLine(database.StringGet("Kumulus"));
+
+			Console.WriteLine(database.StringGet("Kumulus2"));
+			database.StringSet("Kumulus2", "Test Redis2");
+			Console.WriteLine(database.StringGet("Kumulus2"));
+
+			Console.ReadKey();
+
+		}
+
+
+		private static Lazy<ConnectionMultiplexer> lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+		{
+			//Adicione a string Primary connection string (StackExchange.Redis)
+			//Encontra-se na sessão: Access Keys da Azure dentro do serviço Azure Redis Cache
+			string connectionString = "";
+
+			return ConnectionMultiplexer.Connect(connectionString);
+		});
+
+		public static ConnectionMultiplexer Connectio
+		{
+			get
+			{
+				return lazyConnection.Value;
+			}
+		}
+
+		}
+}
